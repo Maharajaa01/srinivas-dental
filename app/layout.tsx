12d7cf1f5268727b12768efde5a462a23464c9c9
Maharaja <114376134+Maharajaa01@users.noreply.github.com>
2025-07-20 01:21:20 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,38 +19,38 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Srinivas Dental...",
-  description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
-
-};
-
 // export const metadata: Metadata = {
-//   title: "Srinivas Dental",
+//   title: "Srinivas Dental...",
 //   description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
-//   openGraph: {
-//     title: "Srinivas Dental",
-//     description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
-//     url: "https://srinivas-dental.vercel.app", // Replace with your actual domain
-//     siteName: "Srinivas Dental",
-//     images: [
-//       {
-//         url: "/og.png", // Provide an image in public folder (ideal: 1200x630)
-//         width: 1200,
-//         height: 630,
-//         alt: "Srinivas Dental Clinic - Quality Smile Care",
-//       },
-//     ],
-//     locale: "en_US",
-//     type: "website",
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "Srinivas Dental",
-//     description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
-//     images: ["/og.png"], // Same image as above
-//   },
+
 // };
+
+export const metadata: Metadata = {
+  title: "Srinivas Dental",
+  description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
+  openGraph: {
+    title: "Srinivas Dental",
+    description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
+    url: "https://srinivas-dental.vercel.app", // Replace with your actual domain
+    siteName: "Srinivas Dental",
+    images: [
+      {
+        url: "/og.png", // Provide an image in public folder (ideal: 1200x630)
+        width: 1200,
+        height: 630,
+        alt: "Srinivas Dental Clinic - Quality Smile Care",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Srinivas Dental",
+    description: "Comprehensive dental care by Dr. Kamalavizhi Srinivas. Trusted by families in Chennai.",
+    images: ["/og.png"], // Same image as above
+  },
+};
 
 
 export default function RootLayout({
