56f7baa20993445fb1b70a0cf8c847088630767c
Maharaja <114376134+Maharajaa01@users.noreply.github.com>
2025-07-20 00:56:01 +0530

    Fix22

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-// 
+// ss
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
